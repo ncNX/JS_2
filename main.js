cc7ddc701f1e528c1cac387c3a12e206da3bd123
Igor <dumanschi.igor@gmail.com>
2024-10-02 21:17:19 +0300

    JS_2

---
+++ main.js
@@ -0,0 +1,31 @@
+// const age = '50';
+// const age2	= 50;
+
+// console.log(Number(age))
+// console.log(+age)
+
+// console.log(age2.toString())
+
+// const userAge = 18
+
+// if(userAge == 18 ) {
+// 	console.log(userAge)
+// } else {
+// 	console.log('1')
+// }
+
+// const userName = 'vasile'
+
+// if(typeof userName === 'string') {
+// 	console.log(userName)
+// }
+
+// const dogAge = 15
+// const catAge = 10
+
+// if(dogAge < catAge) {
+// 	console.log('cainele nu este mai mare')
+// } else {
+// 	console.log('cainele  este mai mare')
+// }
+
